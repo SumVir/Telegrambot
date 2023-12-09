b69adae0902cf576a31488b41a23d0ffce42cf8f
SumVir <138498141+SumVir@users.noreply.github.com>
2023-12-09 17:33:24 +0400

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from typing import Final
+
+# Telegram Bot API Token
+TELEGRAM_TOKEN: Final = '6690162796:AAH0rOSHMs7JeloydISzkRcjt847kzgRYoM'
+BOT_USERNAME: Final = '@H4c6erbot'
+OWNER_ID = "1909135185"
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:john1776@database-1.cerzdgoykjuo.eu-north-1.rds.amazonaws.com:5431/dbname1'
